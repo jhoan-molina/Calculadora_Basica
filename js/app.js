ae15e3149081c77a7cca049a6677972523ff6368
jhoan-molina <56314073+jhoan-molina@users.noreply.github.com>
2022-10-14 00:48:35 -0500

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,12 @@
+function sumar(){
+    //Trae el formulario completo, al igual que el contenido del mismo
+    const forma = document.getElementById('forma');
+    let operandoA = forma['operandoA'];
+    let operandoB = forma['operandoB'];
+    let resultado = parseInt(operandoA.value) + parseInt(operandoB.value);
+    if(isNaN(resultado)){
+        resultado = 'La operación no incluye números';
+    }
+    document.getElementById('resultado').innerHTML = `Resultado: ${resultado}`;
+    console.log(`Resultado: ${resultado}`);
+}
